6db035a7895d2e7d90bce0a359c1c93fcca53871
Praneethachodey <praneethachodey@gmail.com>
2020-09-28 04:33:27 +0530

    [Praneetha]merged: with uc3

--- employee.java
+++ employee.java
@@ -5,15 +5,21 @@ public static void main(String args[])
 	System.out.println("Welcome to employee wage problem");
 	int IS_FULL_TIME=1;
 	int EMP_RATE_PER_HOUR=20;
+	int IS_PART_TIME=2;
 
 	int empHrs=0;
         int empwage=0; 
 	
-	double empCheck = Math.floor(Math.random()*10)%2;
+	double empCheck = Math.floor(Math.random()*10)%3;
 	if(empCheck==IS_FULL_TIME)
 		{
-		System.out.println("Employee is present");
+		System.out.println("Employee is present-full time");
 		empHrs=8;
+		}
+	else if(empCheck==IS_PART_TIME)
+		{
+		System.out.println("Employee is present-part time");
+		empHrs=4;
 		}
 		
 
