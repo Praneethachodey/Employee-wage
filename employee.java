e239b2d4a1001bab2cf0ae79d5ca5f4bc2559fd7
Praneethachodey <praneethachodey@gmail.com>
2020-09-28 04:40:47 +0530

    [Praneetha]refactored; using switch statement

--- employee.java
+++ employee.java
@@ -1,30 +1,29 @@
 public class employee{
-
+	public static final int IS_FULL_TIME=1;
+	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int IS_PART_TIME=2;
 public static void main(String args[])
 {
 	System.out.println("Welcome to employee wage problem");
-	int IS_FULL_TIME=1;
-	int EMP_RATE_PER_HOUR=20;
-	int IS_PART_TIME=2;
-
 	int empHrs=0;
         int empwage=0; 
 	
 	double empCheck = Math.floor(Math.random()*10)%3;
-	if(empCheck==IS_FULL_TIME)
-		{
+	switch((int)empCheck)
+	{
+        case IS_FULL_TIME:
 		System.out.println("Employee is present-full time");
 		empHrs=8;
-		}
-	else if(empCheck==IS_PART_TIME)
-		{
+		break;
+	
+	case IS_PART_TIME:
 		System.out.println("Employee is present-part time");
 		empHrs=4;
-		}
+		break;
 		
-
-	else
+	default:
 		System.out.println("Employee is absent");
+	}
 
 	empwage=empHrs*EMP_RATE_PER_HOUR;
 	System.out.println("employee wage is " + empwage);
