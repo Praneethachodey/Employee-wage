4ea69066b81778e2104d70e8ecfa983cc582f3ea
Praneethachodey <praneethachodey@gmail.com>
2020-09-28 05:21:52 +0530

    [Praneetha]refactored: into a class function

--- employee.java
+++ employee.java
@@ -4,14 +4,13 @@ public class employee{
 	public static final int IS_PART_TIME=2;
 	public static final int NUM_OF_WORKING_DAYS=20;
 	public static final int MAX_HRS_IN_A_MONTH=100;
-public static void main(String args[])
-{
-	System.out.println("Welcome to employee wage problem");
-	int total=0;
+
+public static int calEmpWage(String company, int empRate, int numOfDays,int maxHrs)
+{	int total=0;
  	int totalEmpHrs=0;
         int totalWorkingDays=0;
 	
-	while(totalEmpHrs<MAX_HRS_IN_A_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+	while(totalEmpHrs<maxHrs && totalWorkingDays<numOfDays)
 	{
 		double empCheck = Math.floor(Math.random()*10)%3;
 		totalWorkingDays++;
@@ -32,11 +31,21 @@ public static void main(String args[])
 			System.out.println("Employee is absent");
 		}
 
-		int empwage=empHrs*EMP_RATE_PER_HOUR;
+		int empwage=empHrs*empRate;
 		totalEmpHrs+=empHrs;
        		total+= empwage;
 		System.out.println("employee wage is " + empwage);
 	}
-	System.out.println("total employee wage is : " + total);
+	System.out.println("total employee wage for the company " + company + " is : " + total);
+return total;
+}
+
+
+public static void main(String args[])
+{
+	System.out.println("Welcome to employee wage problem");
+	calEmpWage("Dmart",20,2,10);
+	
+	
 }
 }
